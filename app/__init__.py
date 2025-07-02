c6aa35d75ec26687e4724391820ef037129fe001
Alvaro <alvaro.vaquero@mensoftlabs.com>
2025-07-02 08:18:33 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 # app/__init__.py
 
 # Declaramos el paquete
-# Puedes añadir aquí inicializaciones globales si lo deseas
+# Inicializaciones globales opcionales
 
-# por ahora solo expone los módulos
-from . import models
-from . import styles
-from . import layout
-from . import metrics
-from . import maps
-from . import charts
-from . import footer
+from . import (
+    config,
+    data_load,
+    footer,
+    forecast_utils,
+    map_utils,
+    metrics,
+    models,
+    parcels,
+    styles,
+    utils,
+    visuals
+)
